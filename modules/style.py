72d162f26fbbff74a1d8ffc1f317b9079c86dff9
RoadToHacker <43406476+RoadToHacker@users.noreply.github.com>
2018-09-19 15:51:53 +0200

    Create style.py

---
+++ modules/style.py
@@ -0,0 +1,42 @@
+'''
+- Foxbox Penetration Testing Tool
+
+- Foxbox is and easy and powerful Penetration Testing tool 
+- which allows you to find and scan urls by using dorks.
+
+- This tool is for Educational purposes only.
+- Developers assume no liability and are not responsable
+- for any misuse or damage caused by this program.
+
+- Coded by RoadToHacker
+- Instagram: road.to.hacker
+'''
+
+# Colors Formatting
+
+HEADER = '\033[95m'
+BLUE = '\033[94m'
+GREEN = '\033[92m'
+YELLOW = '\033[93m'
+WHITE = '\033[97m'
+RED = '\033[91m'
+ENDC = '\033[0m'
+BRIGHT = '\033[1m'
+EBRIGHT = '\033[0m'
+
+
+banner = ''' %s
+    888888  dP"Yb  Yb  dP 88""Yb  dP"Yb  Yb  dP 
+    88__   dP   Yb  YbdP  88__dP dP   Yb  YbdP  
+    88""   Yb   dP  dPYb  88""Yb Yb   dP  dPYb  
+    88      YbodP  dP  Yb 88oodP  YbodP  dP  Yb 
+
+	%s-%s Foxbox Penetration Testing tool
+	%s-%s Coded by RoadToHacker
+	%s-%s Version: 0.1
+	%s-%s Github: https://github.com/RoadToHacker
+	%s-%s Instagram: road.to.hacker
+
+[%s!%s] Developers assume no liability and are not responsable 
+    for any misuse or damage caused by this program.
+''' %(RED, BLUE, WHITE, BLUE, WHITE, BLUE, WHITE, BLUE, WHITE, BLUE, WHITE, RED, WHITE)
